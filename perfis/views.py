ceb22973cda46d0e9a5c40b38d4c1c7b6d152727
Guilherme Ferreira <guilherme.ferreira@estudante.ifb.edu.br>
2021-10-26 11:09:58 -0300

    Finaliza o módulo 3

--- perfis/views.py
+++ perfis/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def exibir(request, perfil_id):
+    perfil = Perfil()
+
+    if perfil_id == 1:
+        perfil = Perfil('Guilherme Ferreira',
+                        'guilherme.ferreira@ifb.edu.br', '123456', 'IFB')
+
+    if perfil_id == 2:
+        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla')
+
+    return render(request, 'perfil.html', {'perfil': perfil})
