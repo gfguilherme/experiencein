4c6dd95dcaba49aaf59ebb9adc5b4bd5dd7295a6
Guilherme Ferreira <guilherme.ferreira@estudante.ifb.edu.br>
2021-11-17 14:23:14 -0300

    Finaliza o módulo 5

--- perfis/views.py
+++ perfis/views.py
@@ -3,7 +3,7 @@ from perfis.models import Perfil
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'perfis': Perfil.objects.all()})
 
 
 def exibir(request, perfil_id):
