6a87186f2b1422494c5097348e3554937162be8a
Guilherme Ferreira <guilherme.ferreira@estudante.ifb.edu.br>
2021-11-17 14:00:44 -0300

    Finaliza o módulo 4

--- perfis/views.py
+++ perfis/views.py
@@ -7,13 +7,5 @@ def index(request):
 
 
 def exibir(request, perfil_id):
-    perfil = Perfil()
-
-    if perfil_id == 1:
-        perfil = Perfil('Guilherme Ferreira',
-                        'guilherme.ferreira@ifb.edu.br', '123456', 'IFB')
-
-    if perfil_id == 2:
-        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla')
-
+    perfil = Perfil.objects.get(id=perfil_id)
     return render(request, 'perfil.html', {'perfil': perfil})
